6cb1a7ee29ef059f0f10fe0ddb7f3236b7aefa8f
hozuu <38792466+ho-jun99@users.noreply.github.com>
2023-05-03 23:18:06 +0900

    [Silver I] Title: IOIOI, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -20,50 +20,11 @@ for i in range(n+1) :
   else :
     Pn += "IO"
 
-cursor = 0
+# print(Pn)
+start = 0
 cnt = 0
-result = 0
-while cursor < m-2 :
-  if s[cursor:cursor+3] == "IOI" :
-    cursor +=2
-    cnt +=1
-    if cnt == n :
-      result +=1
-      cnt -=1
-  else :
-    cursor += 1
-    cnt = 0
-
-print(result)
-
-def solve50_same1() :
-  # 순회 한번으로 IOI의 총 갯수를 구함
-  cursor = 0
-  cnt = 0
-  result = 0
-  while cursor < m - 2 :
-    if s[cursor:cursor+3] == "IOI" :
-      cursor += 2
-      cnt +=1
-      if cnt == n :
-        result+=1
-        cursor = cursor - (2*n) + 1
-        cnt = 0
-    else :
-      cnt = 0
-      cursor +=1
-  print(result)
-
-# IOI는 모든 Pn에 포함 된 수이고, n으로 나누어버린다.
-
-
-
-def solve50point():
-  # print(Pn)
-  start = 0
-  cnt = 0
-  while s.find(Pn,start) != -1:
-    cnt +=1
-    start = s.find(Pn,start) +1
-    # print(f"index : {start-1}")
-  print(cnt)
+while s.find(Pn,start) != -1:
+  cnt +=1
+  start = s.find(Pn,start) +1
+  # print(f"index : {start-1}")
+print(cnt)
