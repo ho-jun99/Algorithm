9f0d089b4c4e5e3abb610ae538525c348b35b081
hozuu <38792466+ho-jun99@users.noreply.github.com>
2023-07-29 22:11:23 +0900

    [Bronze III] Title: 부호, Time: 200 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.py
@@ -0,0 +1,15 @@
+import sys
+
+
+for _ in range(3) :
+  t = int(sys.stdin.readline())
+  sum = 0
+  for i in range(t) :
+    sum += int(sys.stdin.readline())
+
+  if sum == 0 :
+    print(0)
+  elif sum > 0 :
+    print("+")
+  else :
+    print("-")
