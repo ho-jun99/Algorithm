48ee33b2e8f763ca642245e396c84d0088d348e7
hozuu <38792466+ho-jun99@users.noreply.github.com>
2023-08-31 23:45:58 +0900

    [Bronze IV] Title: 줄번호, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.py
@@ -0,0 +1,24 @@
+import sys
+
+
+n = int(sys.stdin.readline().strip())
+
+for i in range(1,n+1) :
+  s = sys.stdin.readline().rstrip()
+  print(f"{i}. {s}")
+
+
+"""
+1. Lionel Cosgrove
+2. Alice
+3. Columbus and Tallahassee
+4. Shaun and Ed
+5. Fido
+
+
+1. Lionel Cosgrove
+2. Alice
+3. Columbus and Tallahassee
+4. Shaun and Ed
+5. Fido
+"""
